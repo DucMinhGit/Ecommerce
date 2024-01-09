ea0c15a74aaf5452e56e0732a969c706362c8840
minh hoang <21ducminh@gmail.com>
2024-01-09 13:32:36 +0700

    Remove info default config mongodb

--- src/configs/config.mongodb.js
+++ src/configs/config.mongodb.js
@@ -5,9 +5,9 @@ const dev = {
     port: process.env.DEV_APP_PORT
   },
   db: {
-    host: process.env.DEV_DB_HOST || 'localhost',
-    port: process.env.DEV_DB_PORT || 27017,
-    name: process.env.DEV_DB_NAME || 'shopDEV'
+    host: process.env.DEV_DB_HOST,
+    port: process.env.DEV_DB_PORT,
+    name: process.env.DEV_DB_NAME
   }
 }
 
