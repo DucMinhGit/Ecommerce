c2fbbb28205f2476526016d9b89b0e499e884734
minh hoang <21ducminh@gamil.com>
2024-01-11 21:36:19 +0700

    Sign-up token with JWT

--- src/app.js
+++ src/app.js
@@ -106,6 +106,10 @@ app.use(morgan("dev"));
 
 app.use(helmet());
 app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
 // init db
 require('./dbs/init.mongodb');
--- src/services/access.service.js
+++ src/services/access.service.js
@@ -1,7 +1,11 @@
 'use strict'
 
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 const shopModel = require('../models/shop.model');
+const KeyTokenService = require('./keyToken.service');
+const { createTokenPair } = require('../auth/authUtils');
+const { getInfoData } = require('../utils');
 
 const RoleShop =  {
   SHOP: 'SHOP',
@@ -29,7 +33,47 @@ class AccessService {
       });
 
       if (newShop) {
-        
+        // Created privateKey, publicKey
+        const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+          modulusLength: 4096,
+          publicKeyEncoding: {
+            type: 'pkcs1',
+            format: 'pem'
+          },
+          privateKeyEncoding: {
+            type: 'pkcs1',
+            format: 'pem'
+          }
+        });
+
+        const publicKeyString = await KeyTokenService.createKeyToken({
+          userId: newShop._id,
+          publicKey
+        });
+
+        if(!publicKeyString) {
+          return {
+            code: 'xxx',
+            message: 'publicKeyString error'
+          }
+        }
+        console.log('publicKeyString', publicKeyString);
+        const publicKeyObject = crypto.createPublicKey(publicKeyString);
+
+        const tokens = await createTokenPair({userId: newShop._id, email}, publicKeyObject, privateKey);
+        console.log(`Created Token Success::`, tokens);
+
+        return {
+          code: 201,
+          metadata: {
+            shop: getInfoData({ fields: ['_id', 'name', 'email'], object: newShop }),
+            tokens
+          }
+        }
+      }
+      return {
+        code: 200,
+        metadata: null
       }
     } catch(error) {
       return {
