e8f68c0cb37230367581b3a1df598e0a5f70babc
DucMinhGit <21ducminh@gmail.com>
2024-02-18 11:03:24 +0700

    create route cart

---
+++ src/routes/cart/index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const express = require('express');
+const router = express.Router();
+const cartController = require('../../controllers/cart.controller');
+const { asyncHandler } = require('../../helpers/asyncHandler');
+const { authenticationV2 } = require('../../auth/authUtils');
+
+router.use(authenticationV2);
+router.get('', asyncHandler(cartController.listToCart));
+router.post('', asyncHandler(cartController.addToCart));
+router.post('/update', asyncHandler(cartController.update));
+router.delete('', asyncHandler(cartController.delete));
+
+module.exports = router
